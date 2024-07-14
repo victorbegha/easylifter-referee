19742436541f0903ca42c792ce6718d7c84e7efd
victorbegha <victorbegha@outlook.com>
2024-07-14 12:09:36 -0300

    Added ESP8266 code; update code generator to use it
    Signed-off-by: victorbegha <victorbegha@outlook.com>

--- tools/inoCodeGenerator/minify.js
+++ tools/inoCodeGenerator/minify.js
@@ -5,30 +5,38 @@ Licensed under GPLv3 (https://www.gnu.org/licenses/gpl-3.0.html)
 */
 const fs = require('fs');
 
-function minifyToHtml(htmlString) {
-    const base64favicon = fs.readFileSync('./base64logo.txt').toString();
+function minifyToHtml(htmlString, board) {
+  const base64favicon = fs.readFileSync('./base64logo.txt').toString();
 
-    // TODO: update to handle single-line JavaScript comments
-    htmlString = htmlString.replace(/[\r\n]+/g, ''); // line breaks
-    htmlString = htmlString.replace(/\<\!\-\-.+\-\-\>/g, ''); // HTML comments
-    htmlString = htmlString.replace(/\/\*.+?\*\//g, ''); // JavaScript comments
-    htmlString = htmlString.replace(/[\s\t]+/g, ' '); // Spaces and tabs
-    htmlString = htmlString.replace('<<BASE64ICON>>', base64favicon); // Insert base64 encoded favicon
+  // TODO: update to handle single-line JavaScript comments
+  htmlString = htmlString.replace(/[\r\n]+/g, ''); // line breaks
+  htmlString = htmlString.replace(/\<\!\-\-.+\-\-\>/g, ''); // HTML comments
+  htmlString = htmlString.replace(/\/\*.+?\*\//g, ''); // JavaScript comments
+  htmlString = htmlString.replace(/[\s\t]+/g, ' '); // Spaces and tabs
 
-    return htmlString;
+  if (board == 'ESP32') {
+    // Insert base64 encoded favicon
+    htmlString = htmlString.replace('<<BASE64ICON>>', base64favicon);
+  }
+  else {
+    // ESP8266 does not have enough memory for the favicon
+    htmlString = htmlString.replace('<link rel="icon" href="data:image/x-icon;base64,<<BASE64ICON>>" />', '');
+  }
+
+  return htmlString;
 }
 
 function minifyToQuotedString(str) {
-    str = str.replace(/\"/g, '\\"'); // Escape quotes
-    str = '"' + str + '"'; // Wrap the whole thing in quotes, ready to be used as a string in pages.h
-    return str;
+  str = str.replace(/\"/g, '\\"'); // Escape quotes
+  str = '"' + str + '"'; // Wrap the whole thing in quotes, ready to be used as a string in pages.h
+  return str;
 }
 
-function minifyFileByPath(path) {
-    var fileContents = fs.readFileSync(path).toString();
-    fileContents = minifyToHtml(fileContents);
-    fileContents = minifyToQuotedString(fileContents);
-    return fileContents;
+function minifyFileByPath(path, board) {
+  var fileContents = fs.readFileSync(path).toString();
+  fileContents = minifyToHtml(fileContents, board);
+  fileContents = minifyToQuotedString(fileContents);
+  return fileContents;
 }
 
-module.exports = {minifyToHtml, minifyToQuotedString, minifyFileByPath}
+module.exports = { minifyToHtml, minifyToQuotedString, minifyFileByPath };
