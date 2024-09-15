9369dc8f1b88c846b7fae1e061e3715d25da778f
victorbegha <victorbegha@outlook.com>
2024-09-15 10:53:47 -0300

    Formatted timer input mask code
    Signed-off-by: victorbegha <victorbegha@outlook.com>

--- app-electron/ui/timerInputFormatter.js
+++ app-electron/ui/timerInputFormatter.js
@@ -5,52 +5,52 @@ Licensed under GPLv3 (https://www.gnu.org/licenses/gpl-3.0.html)
 */
 
 function clearInput(i) {
-  i.value = "";
+  i.value = '';
 }
 
-function mascara(i) {
+function maskTimerInput(i) {
   var v = i.value;
 
   if (isNaN(v[v.length - 1])) {
-    // impede entrar outro caractere que não seja número
+    // prevents inputting characters that aren't numbers
     i.value = v.substring(0, v.length - 1);
     return;
   }
 
-  i.setAttribute("maxlength", "5");
+  i.setAttribute('maxlength', '5');
   if (v.length == 2) {
-    i.value += ":";
+    i.value += ':';
   } else if (v.length > 2) {
-    v = v.replace(":", "");
-    v = v.substring(0, 2) + ":" + v.substring(2);
+    v = v.replace(':', '');
+    v = v.substring(0, 2) + ':' + v.substring(2);
     i.value = v;
   }
 }
 
-timerInput.addEventListener("keydown", function (e) {
+timerInput.addEventListener('keydown', function (e) {
   // If pressing backspace when ':' is the last character, erase it as well as the previous character. The ':' should
   // act only as a mask and should be intangible for the user
-  if (e.key === "Backspace" && this.value.includes(":") && this.value[this.value.length - 1] == ":") {
+  if (e.key === 'Backspace' && this.value.includes(':') && this.value[this.value.length - 1] == ':') {
     this.value = this.value.substring(0, this.value.length - 1);
   }
 });
 
 function validateTimer(i) {
-  if (i.value == null || i.value == "" || i.value.length > 5) {
-    i.value = "01:00";
+  if (i.value == null || i.value == '' || i.value.length > 5) {
+    i.value = '01:00';
   } else if (i.value.length < 5) {
-    i.value = i.value + "00:00".substring(i.value.length, 5);
+    i.value = i.value + '00:00'.substring(i.value.length, 5);
   } else {
     let minutesString = i.value.substring(0, 2);
     let secondsString = i.value.substring(3, 5);
     let minutes = parseInt(minutesString);
     let seconds = parseInt(secondsString);
     if (minutes > 59) {
-      minutesString = "59";
+      minutesString = '59';
     }
     if (seconds > 59) {
-      secondsString = "59";
+      secondsString = '59';
     }
-    i.value = minutesString + ":" + secondsString;
+    i.value = minutesString + ':' + secondsString;
   }
 }
